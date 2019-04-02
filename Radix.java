51ceb88eb048cf06e103519bf22f9d74482cf126
Joycevw02 <43048262+Joycevw02@users.noreply.github.com>
2019-04-02 14:48:38 -0400

    Testing some things

--- Radix.java
+++ Radix.java
@@ -1,5 +1,14 @@
 public class Radix{
   public static void radixsort(int[]data){
+    MyLinkedList<Integer> bucket = new MyLinkedList<Integer>();
+    for (int i = 0; i < 10; i ++){
+      bucket.add(i);
+    }
+    System.out.println(bucket);
+  }
   
+  public static void main(String[] args){
+    int[] data = {1,6,8,2,4,6,38,234,543};
+    radixsort(data);
   }
 }
